74fee98b0bb06edf0c0187f14b654de5cf92fc1f
Jérémy James <jeremy.james@kozea.fr>
2012-04-13 16:32:21 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Pynuts",
+    url="http://www.pynuts.org/",
+    license="GNU GPL v3",
+    platforms="Any",
+    packages=["pynuts"],
+    provides=["pynuts"])
