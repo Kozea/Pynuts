84ebea54e259edb6f558257943691362932eedbf
Jérémy James <jeremy.james@kozea.fr>
2012-04-16 11:21:46 +0200

    Fix __init__ config and reflect

--- pynuts/__init__.py
+++ pynuts/__init__.py
@@ -3,10 +3,10 @@ from flaskext.sqlalchemy import SQLAlchemy
 
 
 class Pynuts(flask.Flask):
-    def __init__(self, *args, **kwargs):
-        config = kwargs.pop('config', {})
-        super(Pynuts, self).__init__(*args, **kwargs)
-        self.config.update(config)
+    def __init__(self, import_name, config=None, reflect=False,
+                 *args, **kwargs):
+        super(Pynuts, self).__init__(import_name, *args, **kwargs)
+        self.config.update(config or {})
         self.db = SQLAlchemy(self)
-        self.db.metadata.reflect(bind=self.db.get_engine(self))
-        self.db.Model._session = self.db.session
+        if reflect:
+            self.db.metadata.reflect(bind=self.db.get_engine(self))
