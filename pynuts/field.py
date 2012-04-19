3eb366784ae4cd94b3929c356f843b24bb326c56
Jérémy James <jeremy.james@kozea.fr>
2012-04-19 15:49:46 +0200

    Add contenteditable field

---
+++ pynuts/field.py
@@ -0,0 +1,17 @@
+from wtforms import StringField
+from wtforms.widgets import html_params, HTMLString
+
+
+class Editable(object):
+    """Contenteditable widget."""
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        return HTMLString(
+            u'<div contenteditable="true" %s>%s</div>' %
+            (html_params(name=field.name, **kwargs),
+             unicode(field._value())))
+
+
+class EditableField(StringField):
+    """Contenteditable field."""
+    widget = Editable()
