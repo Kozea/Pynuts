92639a4252fb524e68d7fb503a2d4a9749fcdb5b
Jérémy James <jeremy.james@kozea.fr>
2012-04-27 14:54:39 +0200

    Simple example is simple.

--- doc/example/simple/application.py
+++
@@ -1,7 +0,0 @@
-import pynuts
-
-CONFIG = {
-        'CSRF_ENABLED': False,
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:////tmp/test.db'}
-
-app = pynuts.Pynuts(__name__, config=CONFIG)
--- doc/example/simple/executable.py
+++ doc/example/simple/executable.py
@@ -1,16 +1,46 @@
-from application import app
-import view
+from flaskext.wtf import (Form, TextField, Required)
+from sqlalchemy.ext.hybrid import hybrid_property
+
+import pynuts
+
+CONFIG = {
+        'CSRF_ENABLED': False,
+        'SQLALCHEMY_DATABASE_URI': 'sqlite:////tmp/test.db'}
+
+app = pynuts.Pynuts(__name__, config=CONFIG)
+
+
+class Employee(app.db.Model):
+        __tablename__ = 'Employee'
+        id = app.db.Column(app.db.Integer(), primary_key=True)
+        name = app.db.Column(app.db.String())
+        firstname = app.db.Column(app.db.String())
+
+        @hybrid_property
+        def fullname(self):
+            return '%s - %s' % (self.firstname, self.name)
+
+
+class EmployeeView(app.ModelView):
+    model = Employee
+
+    list_column = 'fullname'
+    create_columns = ('name', 'firstname')
+
+    class Form(Form):
+        name = TextField(u'Surname', validators=[Required()])
+        firstname = TextField(u'Firstname', validators=[Required()])
 
 
 @app.route('/')
 @app.route('/employees/')
 def employees():
-    return view.EmployeeView.list('list_employees.html')
+    return EmployeeView.list('list_employees.html')
 
 
 @app.route('/employee/add/', methods=('POST', 'GET'))
 def add_employee():
-    return view.EmployeeView().create('add_employee.html',
+    return EmployeeView().create('add_employee.html',
                                       redirect='employees')
 
 
