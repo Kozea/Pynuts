a069d27a8d36518f2e40cec115a6ae6385156d78
Aymeric Bois <aymeric.bois@kozea.fr>
2012-05-02 10:12:15 +0200

    Remove useless report folder

--- doc/example/advanced/document.py
+++ doc/example/advanced/document.py
@@ -3,6 +3,6 @@ from application import app
 
 class EmployeeDoc(app.Document):
 
-    model = 'models/report'
+    model = 'models/'
     document_id_template = '{employee.data.id}'
     repository = '/tmp/employees.git'
