ce9acbcd4e9c0bf8d5eb9e1f7a958f3e94ffdec0
Simon Sapin <simon.sapin@exyr.org>
2012-05-03 17:15:13 +0200

    Add easy writing to git, rewrite most of the module, API changes

--- pynuts/git.py
+++ pynuts/git.py
@@ -1,160 +1,234 @@
+# coding: utf8
 import os
 import time
-import errno
-import functools
 
+import jinja2
+from werkzeug.utils import cached_property
 from dulwich.repo import Repo, Blob, Tree, Commit
-from werkzeug.exceptions import NotFound as werkzeug_NotFound
-from jinja2 import BaseLoader, TemplateNotFound
 
 
-class FileNotFound(IOError):
-    """No such file or directory."""
+class GitException(Exception):
+    """Base class for git-related exceptions."""
 
 
-class NotAFile(IOError):
-    """open() on a directory or other non-file object."""
+class NotFoundError(GitException):
+    """No such file or directory (blob or tree)."""
 
 
-class NotADirectory(IOError):
-    """listdir() on a file or other non-directory object."""
+class ObjectTypeError(GitException):
+    """For example, found a blob where a tree was expected."""
 
 
-def convert_errors(function):
-    @functools.wraps(function)
-    def wrapper(self, path):
-        try:
-            return function(self, path)
-        except (OSError, IOError) as exc:
-            if exc.errno == errno.ENOENT:  # No such file or directory
-                raise FileNotFound(path)
-            elif exc.errno == errno.EISDIR:  # Is a directory
-                raise NotAFile(path)
-            elif exc.errno == errno.ENOTDIR:  # Not a directory
-                raise NotADirectory(path)
-            else:
-                raise
-        except werkzeug_NotFound:
-            raise FileNotFound(path)
-    return wrapper
+class NoSuchBranchError(GitException):
+    """Operation on a branch that does not exist."""
 
 
-class GitFS(object):
-    def __init__(self, repository, branch, commit=None):
-        self.repository = Repo(repository)
-        self.store = self.repository.object_store
-        if not commit:
-            if branch not in self.repository.refs:
-                # The branch does not exist yet
-                self.commit = self.tree = None
-                return
-            commit = self.repository.refs[branch]
-        self.commit = self.repository.commit(commit)
-        self.tree = self.repository.tree(self.commit.tree)
+class ConflictError(GitException):
+    """Operation on a branch that does not exist."""
+
+
+class Git(object):
+    """Represents a commit and its tree in a git repository.
+
+    This accesses the repository directly without "checking out" files
+    into a "working directory".
+
+    This is a higher level wrapper around Dulwich for bare repositories.
+
+    :param repository_path:
+        Full path to the repository’s directory in the filesystem.
+    :param branch:
+        Which branch to use in the repository. (``refs/heads`` is implicit.)
+    :param commit_id:
+        The SHA1 hash of the commit to use. If given, no check is made that
+        the commit is actually reachable from ``branch``. If not given,
+        use the latest commit in ``branch``.
+
+    """
+
+    committer = 'Pynuts <pynuts@pynuts.org>'
+
+    def __init__(self, repository_path, branch, commit=None):
+        self.repository_path = repository_path
+        self.repository = Repo(repository_path)
+        self._add_object = self.repository.object_store.add_object
+        self.ref = 'refs/heads/' + branch
+        commit = commit or self.repository.refs.read_ref(self.ref)
+        if commit:
+            self.head = self.repository.commit(commit)
+            self.tree = self.repository.tree(self.head.tree)
+        else:
+            self.head = None
+            self.tree = Tree()
+
+    def jinja_loader(self, sub_directory=None):
+        prefix = sub_directory + '/' if sub_directory else ''
+
+        def get_source(environment, template):
+            path = prefix + template
+            try:
+                source = self.read(path).decode('utf-8')
+            except NotFoundError as err:
+                raise jinja2.TemplateNotFound(template)
+            # Fake filename for tracebacks:
+            filename = '%s/<git commit %s>/%s' % (
+                self.repository_path, self.head.id, path)
+            # Commits are immutable:
+            is_uptodate = lambda: True
+            return source, filename, is_uptodate
+
+        loader = jinja2.BaseLoader()
+        loader.get_source = get_source
+        return loader
 
     def history(self):
-        if self.commit:
-            commit = self.commit
-            while 1:
-                yield commit.id
-                if not commit.parents:
-                    break
-                commit = self.repository.commit(commit.parents[0])
+        """Return an iterable of commit IDs, starting from this one.
 
-    def filename(self, path):
-        return '<commit %s>/%s' % (self.commit.id, path)
+        For merge commits, only the first parent is followed.
+        The history is empty if the branch does not exist yet.
+
+        """
+        commit = self.head
+        if not commit:
+            return
+        while 1:
+            yield commit.id
+            if not commit.parents:
+                break
+            commit = self.repository.commit(commit.parents[0])
+
+    def _split_path(self, path):
+        return [part for part in path.encode('utf8').split('/') if part]
 
     def _lookup(self, path):
-        parts = [part for part in path.split('/') if part]
+        """Recursively walk Tree objects and return the object at this path."""
+        parts = self._split_path(path)
         tree = self.tree
         if not parts:
             return tree
-        get_tree = self.repository.tree
+        get_object = self.repository.get_object
         for i, part in enumerate(parts):
             if i:
-                tree = get_tree(sha)
+                tree = get_object(sha)
+                if tree.type_name != 'tree':
+                    raise ObjectTypeError(
+                        'Expected a tree, found a %s at %s.'
+                        % (tree.type_name, '/'.join(parts[:i])))
             try:
                 mode, sha = tree[part]
             except KeyError:
-                raise FileNotFound(path)
-        return self.repository.get_object(sha)
+                raise NotFoundError(path)
+        return get_object(sha)
 
-    def store_commit(self, tree_id, parents, author, message):
+    def read(self, path):
+        """Return as a byte string the content of the blob at ``path``.
+
+        :raises: NotFoundError, ObjectTypeError
+
+        """
+        blob = self._lookup(path)
+        if blob.type_name != 'blob':
+            raise ObjectTypeError(
+                'Expected a blob, found a %s at %s.' % (blob.type_name, path))
+        return blob.data
+
+    def write(self, path, bytestring):
+        """Update in place self.tree and make sure everything is stored."""
+        parts = self._split_path(path)
+        if not parts:
+            raise ValueError('Empty path: %r' % path)
+        tree = self.tree
+        traversed_trees = []
+        get_object = self.repository.get_object
+        for i, name in enumerate(parts[:-1]):
+            try:
+                mode, sha = tree[name]
+            except KeyError:
+                sub_tree = Tree()
+            else:
+                sub_tree = get_object(sha)
+                if tree.type_name != 'tree':
+                    raise ObjectTypeError(
+                        'Expected a tree, found a %s at %s.'
+                        % (tree.type_name, '/'.join(parts[:i])))
+            traversed_trees.append((tree, name, sub_tree))
+            tree = sub_tree
+
+        # TODO: This will also override trees. Raise instead?
+        tree[parts[-1]] = 0100644, self.store_bytes(bytestring)
+        self._add_object(tree)
+
+        for tree, name, sub_tree in reversed(traversed_trees):
+            tree[name] = 040000, sub_tree.id
+            self._add_object(tree)
+
+    def commit(self, author_name, author_email, message):
+        """Add a new commit in the current branch with this one (if any)
+        as a parent, and check that there is no conflict.
+
+        :raises: ConflictError
+
+        """
+        new_commit = self.store_commit(
+            self.tree.id, author_name, author_email, message,
+            parents=[self.head.id] if self.head else [])
+        refs = self.repository.refs
+        if self.head:
+            if not refs.set_if_equals(self.ref, self.head.id, new_commit.id):
+                raise ConflictError('%s is not the last commit in %s.'
+                                    % (self.head.id, self.ref))
+        else:
+            if not refs.add_if_new(self.ref, new_commit.id):
+                raise ConflictError('%s already exists.' % self.ref)
+        self.head = new_commit
+
+    def store_commit(self, tree_id, author_name, author_email,
+                     message, parents):
+        """Store a new commit and return its ID.
+
+        Note that no branch will point to this commit. You may want to use
+        ``commit_into_branch`` instead.
+
+        """
+        author = '%s <%s>' % (author_name, author_email)
         commit = Commit()
-        commit.author = author
-        commit.committer = 'Pynuts'
+        commit.author = author.encode('utf8')
+        commit.committer = self.committer
         commit.author_time = commit.commit_time = int(time.time())
+        # TODO: use the server’s timezone? Something else?
         commit.author_timezone = commit.commit_timezone = 0
-        commit.message = message
+        commit.message = message.encode('utf8')
         commit.tree = tree_id
-        commit.parents = parents or []
-        self.store.add_object(commit)
-        return commit.id
+        commit.parents = parents
+        self._add_object(commit)
+        return commit
 
     def store_directory(self, root):
+        """Recursively store a directory and its content as a tree and
+        return its ID.
+
+        Symbolic links are followed. Other special files are ignored
+
+        """
         tree = Tree()
         for name in os.listdir(root):
             fullname = os.path.join(root, name)
             if os.path.isdir(fullname):
-                sub_id = self.store_directory(fullname)
-                mode = 040000
+                tree.add(name, 040000, self.store_directory(fullname))
             elif os.path.isfile(fullname):
-                sub_id = self.store_file(fullname)
-                mode = 0100644
-            else:
-                continue
-            tree.add(name, mode, sub_id)
-        self.store.add_object(tree)
+                tree.add(name, 0100644, self.store_file(fullname))
+            #else: Ignore special files.
+        self._add_object(tree)
         return tree.id
 
     def store_file(self, filename):
-        blob = Blob.from_string(open(filename).read())
-        self.store.add_object(blob)
-        return blob.id
+        """Store a file as a blob and return its ID."""
+        # TODO: store by chunks without reading everything in memory?
+        with open(filename, 'rb') as fd:
+            return self.store_bytes(fd.read())
 
-    def store_string(self, bytestring):
+    def store_bytes(self, bytestring):
+        """Store a byte string as a blob and return its ID."""
         blob = Blob.from_string(bytestring)
-        self.store.add_object(blob)
+        self._add_object(blob)
         return blob.id
-
-    def listdir(self, path):
-        tree = self._lookup(path)
-        if tree.type_name != 'tree':
-            raise NotADirectory(path)
-        return list(tree)
-
-    def read(self, path):
-        blob = self._lookup(path)
-        if blob.type_name != 'blob':
-            raise NotAFile(path)
-        return blob.data
-
-    def isdir(self, path):
-        try:
-            return self._lookup(path).type_name == 'tree'
-        except FileNotFound:
-            return False
-
-    def isfile(self, path):
-        try:
-            return self._lookup(path).type_name == 'blob'
-        except FileNotFound:
-            return False
-
-    def getmtime(self, path):
-        self._lookup(path)  # raise if not found
-        return self.commit.commit_time
-
-
-class GitLoader(BaseLoader):
-    def __init__(self, git):
-        self.git = git
-
-    def get_source(self, environment, template):
-        if not self.git.isfile(template):
-            raise TemplateNotFound(template)
-        mtime = self.git.getmtime(template)
-        source = self.git.read(template).decode('utf-8')
-        # filename might be None
-        return source, self.git.filename(template), lambda: \
-            self.git.getmtime(template) == mtime
