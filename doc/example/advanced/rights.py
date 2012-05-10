4de2b1ece208b734abd52fd8cd9750147041d910
Jérémy James <jeremy.james@kozea.fr>
2012-05-10 11:27:59 +0200

    Add rights to documentation

--- doc/example/advanced/rights.py
+++ doc/example/advanced/rights.py
@@ -19,7 +19,7 @@ def connected():
 
 @acl
 def connected_user():
-    """Returns whether the user is connected."""
+    """Returns the connected user."""
     if app.context.person:
         return app.context.person == request.view_args.get('person_id')
 
