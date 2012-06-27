1f9a46a92cc535f115bdc10b28e6ac881b07afd6
Florian Mounier <florian.mounier@kozea.fr>
2012-06-27 12:11:56 +0200

    Change jinja environment scope

--- pynuts/__init__.py
+++ pynuts/__init__.py
@@ -6,6 +6,7 @@ from werkzeug import cached_property
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from . import document, git, rights, view, filters
+from .environment import create_environment
 
 
 class Pynuts(flask.Flask):
@@ -65,6 +66,7 @@ class Pynuts(flask.Flask):
         class ModelView(view.ModelView):
             """Model view base class of the application."""
             _pynuts = self
+            environment = create_environment(_pynuts.jinja_env.loader)
 
         self.ModelView = ModelView
 
