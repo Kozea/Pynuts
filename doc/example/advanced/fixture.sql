cecaab2977322628fef14792ba125984c7881fa6
Balthazar Rouberol <balthazar.rouberol@kozea.fr>
2012-10-22 12:46:04 +0200

    Bugfix/Feature: SQL script to populate database with a user
    The example could no be fully tested, as all actions required to be logged in,
    and no user creation procedure was implemented.
    
    The tester will have to execute the test manually with the command
    $ sqlite3 /tmp/test.db < fixture.sql
    after having executed the `executable.py` script
    
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@kozea.fr>

---
+++ doc/example/advanced/fixture.sql
@@ -0,0 +1,2 @@
+-- Populate the Employee database with an user "admin"
+INSERT INTO Employee (person_id, name, firstname, login, password, company_id) VALUES (NULL, "admin", "admin", "admin", "adminpwd", NULL);
