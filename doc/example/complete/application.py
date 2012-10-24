5e5b5d2c370d99c20d2c565627ad25db17ca3a52
Balthazar Rouberol <balthazar.rouberol@kozea.fr>
2012-10-24 12:14:40 +0200

    No need of manual specification of Git repo location
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@kozea.fr>

--- doc/example/complete/application.py
+++ doc/example/complete/application.py
@@ -1,6 +1,5 @@
 import pynuts
 
-CONFIG = {'SQLALCHEMY_DATABASE_URI': 'sqlite:////tmp/test.db',
-          'PYNUTS_DOCUMENT_REPOSITORY': '/tmp/documents.git'}
+CONFIG = {'SQLALCHEMY_DATABASE_URI': 'sqlite:////tmp/test.db'}
 
 app = pynuts.Pynuts(__name__, config=CONFIG)
