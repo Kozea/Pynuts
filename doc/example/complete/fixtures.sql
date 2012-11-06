ab6a2ec9b94acefbe5c7192e4f368688118a54d6
Balthazar Rouberol <balthazar.rouberol@kozea.fr>
2012-11-06 15:58:30 +0100

    Execute SQL script to pre-populate the DW with an admin account
    Signed-off-by: Balthazar Rouberol <balthazar.rouberol@kozea.fr>

---
+++ doc/example/complete/fixtures.sql
@@ -0,0 +1,2 @@
+-- Populate the Employee database with an user "admin"
+INSERT INTO Employee (person_id, name, firstname, login, password, resume, photo, company_id) VALUES (NULL, "admin", "admin", "admin", "adminpwd", NULL, NULL, NULL);
