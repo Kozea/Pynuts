ef285c7b7f015033da72f5e8ebe94b00ec9a5de4
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-02-05 16:32:57 +0100

    Fix SQL again

--- tests/sql/database.sql
+++ tests/sql/database.sql
@@ -3,30 +3,28 @@ BEGIN;
 -- Tables
 
 CREATE TABLE Employee (
-	person_id INTEGER NOT NULL, 
-	name VARCHAR, 
-	firstname VARCHAR, 
-	login VARCHAR, 
-	"order" INTEGER, 
-	password VARCHAR, 
+	person_id INTEGER PRIMARY KEY,
+	"name" VARCHAR,
+	firstname VARCHAR,
+	login VARCHAR,
+	"order" INTEGER,
+	password VARCHAR,
 	company_id INTEGER,
 	driving_license INTEGER,
 	photo VARCHAR,
 	resume VARCHAR,
-	reflected VARCHAR, 
-	PRIMARY KEY (person_id), 
-	FOREIGN KEY(company_id) REFERENCES Company (company_id)
+	reflected VARCHAR,
+    FOREIGN KEY(company_id) REFERENCES Company(company_id)
 );
 
 CREATE TABLE Company (
-	company_id INTEGER NOT NULL, 
-	name VARCHAR,
-	PRIMARY KEY (company_id)
+	company_id INTEGER PRIMARY KEY,
+	"name" VARCHAR
 );
 
 INSERT INTO company VALUES (1, 'Test Company 1');
 
-INSERT INTO employee (person_id, name, firstname, login, password, driving_license, "order", resume, photo, company_id) VALUES
+INSERT INTO employee (person_id, "name", firstname, login, password, driving_license, "order", resume, photo, company_id) VALUES
     (1, 'admin', 'admin', 'admin', 'root', 1, 1, NULL, NULL, NULL),
     (2, 'Tester', 'Tester', 'test_login', 'test_psswd', 0, 2, NULL, NULL, NULL),
     (3, 'Tester', 'Hired', 'hired_tester', 'hired_tester_psswd', 1, 3, NULL, NULL, 1);
