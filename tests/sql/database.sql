2aad4cb159cf93cd615e1af17b720f2ad5214d01
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-02-05 17:17:41 +0100

    Fix SQL again!!!!

--- tests/sql/database.sql
+++ tests/sql/database.sql
@@ -24,9 +24,8 @@ CREATE TABLE Company (
 
 INSERT INTO company VALUES (1, 'Test Company 1');
 
-INSERT INTO employee (person_id, "name", firstname, login, password, driving_license, "order", resume, photo, company_id) VALUES
-    (1, 'admin', 'admin', 'admin', 'root', 1, 1, NULL, NULL, NULL),
-    (2, 'Tester', 'Tester', 'test_login', 'test_psswd', 0, 2, NULL, NULL, NULL),
-    (3, 'Tester', 'Hired', 'hired_tester', 'hired_tester_psswd', 1, 3, NULL, NULL, 1);
+INSERT INTO employee (person_id, "name", firstname, login, password, driving_license, "order", resume, photo, company_id) VALUES (1, 'admin', 'admin', 'admin', 'root', 1, 1, NULL, NULL, NULL);
+INSERT INTO employee (person_id, "name", firstname, login, password, driving_license, "order", resume, photo, company_id) VALUES (2, 'Tester', 'Tester', 'test_login', 'test_psswd', 0, 2, NULL, NULL, NULL);
+INSERT INTO employee (person_id, "name", firstname, login, password, driving_license, "order", resume, photo, company_id) VALUES (3, 'Tester', 'Hired', 'hired_tester', 'hired_tester_psswd', 1, 3, NULL, NULL, 1);
 
 COMMIT;
